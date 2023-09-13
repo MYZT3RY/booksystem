afedd8adb9cea5fed22ea0e3ebf3f6bb69e40b57
MYZT3RY <d1maz.d@yandex.ru>
2023-09-13 23:52:39 +0300

    Authorization form design and logic

---
+++ authorization.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace bookSystem {
+    /// <summary>
+    /// Логика взаимодействия для authorization.xaml
+    /// </summary>
+    public partial class authorization : Window {
+        private string __password = "1234";
+
+        public authorization() {
+            InitializeComponent();
+        }
+
+        private void logIn_Click(object sender, RoutedEventArgs e) {
+            if (password.Password != string.Empty) {
+                if (password.Password == __password) {
+                    this.Close();
+
+                    MainWindow mainWindow = (MainWindow)this.Owner;
+                    mainWindow.enableDataGridContextMenu();
+                }
+                else {
+                    MessageBox.Show("Неправильный пароль!");
+                }
+            }
+        }
+    }
+}
