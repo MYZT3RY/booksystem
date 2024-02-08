f5ea98630ac90ad7caa4d28e6e86f7f873a3b3e5
MYZT3RY <d1maz.d@yandex.ru>
2024-02-08 15:42:52 +0300

    Функционал окна добавления автора

---
+++ authorsAdd.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+
+namespace bookSystem {
+    public partial class authorsAdd : Window {
+        public authorsAdd() {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e) {
+            if (authorName.Text != null) {
+                data.Author author = data.authors.Find(a => a.Author_Name == authorName.Text);
+
+                if (author.Author_Name != null) {
+                    MessageBox.Show("Автор уже существует в базе данных!");
+                }
+                else {
+                    database db = new database();
+
+                    if (db.openConnection(db.connectionString)) {
+                        db.authorAdd(authorName.Text);
+
+                        db.loadAuthors();
+
+                        db.closeConnection();
+
+                        authorsView authorsView = (authorsView)this.Owner;
+                        authorsView.dataGridSetItemSource();
+
+                        this.Close();
+                    }
+                    else {
+                        MessageBox.Show("Подключение к базе данных неактивно!");
+                    }
+                }
+            }
+            else {
+                MessageBox.Show("Необходимо указать автора!");
+            }
+        }
+    }
+}
