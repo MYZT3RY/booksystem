16ada1fa39d6e449484e372a4703948d3d6cdccb
d1maz <d1maz.d@yandex.ru>
2024-02-12 16:44:42 +0300

    Функционал окна просмотра корзины

--- cartView.xaml.cs
+++ cartView.xaml.cs
@@ -1,24 +1,140 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
+using System;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace bookSystem {
-    /// <summary>
-    /// Логика взаимодействия для cartView.xaml
-    /// </summary>
     public partial class cartView : Window {
+        List<data.Book> cartBooks = new List<data.Book>();
+
+        string cheque;
+
         public cartView() {
             InitializeComponent();
+
+            refreshLists();
+        }
+
+        private void getUserBooks() {
+            List<data.Carts> __cartBooks = data.carts.FindAll(c => c.User_Id == data.currentUser.User_Id);
+
+            cartBooks.Clear();
+
+            foreach (data.Carts cart in __cartBooks) {
+                cartBooks.Add(data.books.Find(b => b.Book_Id == cart.Book_Id));
+            }
+        }
+
+        public void dataGridSetItemSource(List<data.Book> book) {
+            dataGrid.ClearValue(ItemsControl.ItemsSourceProperty);
+            dataGrid.ItemsSource = book;
+        }
+
+        private void dataGrid_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            if (dataGrid.SelectedItem != null) {
+                bookView bookView = new bookView((data.Book)dataGrid.SelectedItem);
+
+                bookView.Owner = this;
+                bookView.ShowDialog();
+            }
+        }
+
+        private void Window_Activated(object sender, System.EventArgs e) {
+            refreshLists();
+        }
+
+        private void placeOrderButton_Click(object sender, RoutedEventArgs e) {
+            if (cartBooks.Count > 0) {
+                cheque = generateCheque();
+
+                if (printChequeCheckBox.IsChecked == true) {
+                    printCheque();
+                }
+
+                database db = new database();
+
+                if (db.openConnection(db.connectionString)) {
+                    List<data.Carts> __cartBooks = data.carts.FindAll(c => c.User_Id == data.currentUser.User_Id);
+
+                    foreach (data.Carts c in __cartBooks) {
+                        db.userBookAdd(data.currentUser.User_Id, c.Book_Id);
+                    }
+
+                    db.loadUserBooks();
+
+                    db.closeConnection();
+
+                    clearCart();
+                }
+            }
+        }
+
+        private void cleanCartButton_Click(object sender, RoutedEventArgs e) {
+            if (cartBooks.Count > 0) {
+                clearCart();
+            }
+        }
+
+        private void refreshLists() {
+            getUserBooks();
+
+            dataGridSetItemSource(cartBooks);
+        }
+
+        private string generateCheque() {
+            string __cheque = $"Чек: {Guid.NewGuid()}\n" +
+                              $"Дата: {DateTime.Now.ToString("G")}\n" +
+                               "*************************************\n" +
+                              $"Товар\n";
+
+            foreach (var item in cartBooks) {
+                __cheque += $"{item.Book_Name}\n";
+            }
+
+            __cheque += "*************************************";
+
+            return __cheque;
+        }
+
+        private void printCheque() {
+            PrintDialog printDialog = new PrintDialog();
+
+            if (printDialog.ShowDialog() == true) {
+                FlowDocument flowDocument = new FlowDocument();
+
+                foreach (string line in cheque.Split('\n')) {
+                    Paragraph paragraph = new Paragraph();
+
+                    paragraph.Margin = new Thickness(0);
+                    paragraph.Inlines.Add(new Run(line));
+                    paragraph.FontFamily = new System.Windows.Media.FontFamily("Times New Roman");
+
+                    flowDocument.Blocks.Add(paragraph);
+                }
+
+                DocumentPaginator paginator = ((IDocumentPaginatorSource)flowDocument).DocumentPaginator;
+
+                printDialog.PrintDocument(paginator, this.Title);
+            }
+        }
+
+        private void clearCart() {
+            database db = new database();
+
+            if (db.openConnection(db.connectionString)) {
+                List<data.Carts> __cartBooks = data.carts.FindAll(c => c.User_Id == data.currentUser.User_Id);
+
+                foreach (data.Carts c in __cartBooks) {
+                    db.cartRemove(c);
+                }
+
+                db.loadCart();
+
+                refreshLists();
+
+                db.closeConnection();
+            }
         }
     }
 }
